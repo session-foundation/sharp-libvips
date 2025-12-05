59634ee573715e4a20a13cf781009fdbd6d9b426
Audric Ackermann <audric@getsession.org>
2025-12-05 11:03:53 +1100

    chore: test with npm pack

--- package-npm-packages.sh
+++ package-npm-packages.sh
@@ -5,6 +5,11 @@ ls -la
 cd npm
 ls -la
 
-for dir in */; do
-    [ -d "$dir" ] && tar -cvzf "../sharp-libvips-${dir%/}-1.2.6.tar.gz" -C "$dir" .
-done
+mkdir ${GITHUB_WORKSPACE}/dist
+npm pack --workspaces --pack-destination=${GITHUB_WORKSPACE}/dist
+
+ls -la ${GITHUB_WORKSPACE}/dist
+pwd
+
+cd ../
+npm pack --pack-destination=${GITHUB_WORKSPACE}/dist
