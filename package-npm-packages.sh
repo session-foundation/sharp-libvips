626536c0cb4d203bad74dff743779b3c432aa87c
Audric Ackermann <audric@getsession.org>
2025-12-08 16:03:59 +1100

    fix: rename .tgz to .tar.gz after build & remove img- prefix

--- package-npm-packages.sh
+++ package-npm-packages.sh
@@ -12,3 +12,19 @@ ls -la ${GITHUB_WORKSPACE}/dist
 pwd
 
 npm pack --pack-destination=${GITHUB_WORKSPACE}/dist
+
+echo "Files before renaming:"
+ls -la ${GITHUB_WORKSPACE}/dist
+
+echo ""
+echo "Renaming files (removing 'img-' prefix and changing .tgz to .tar.gz)..."
+for file in ${GITHUB_WORKSPACE}/dist/img-*.tgz; do
+  newname=$(basename "$file" .tgz | sed 's/^img-//').tar.gz
+  echo "  $(basename "$file") → $newname"
+  mv "$file" "${GITHUB_WORKSPACE}/dist/$newname"
+done
+
+echo ""
+echo "Files after renaming:"
+ls -la ${GITHUB_WORKSPACE}/dist
+
